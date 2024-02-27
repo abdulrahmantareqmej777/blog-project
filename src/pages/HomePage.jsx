a960202efb785715e60ee75d8ad08c36a1530172
abdulrahmantareqmej.777 <160115263+abdulrahmantareqmej777@users.noreply.github.com>
2024-02-27 06:40:02 +0000

    2-set up

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainLayout from '../components/MainLayout'
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainLayout>
+            
+        </MainLayout>
+    </div>
+  )
+}
+
+export default HomePage
